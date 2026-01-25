b32fcfcf13ada12a2f72cdb8a8c368377b486e2b
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-01-26 00:24:28 +1100

    feat(tetris): set up game loop

--- tetris_ncurses/main.c
+++ tetris_ncurses/main.c
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-int getkey_block(void) {
-    nodelay(stdscr, false);
-    return getch();
-}
+void display_board(WINDOW *w);
+void display_next(WINDOW *w);
+void display_hold(WINDOW *w);
+void display_score(WINDOW *w);
 
-int getkey_nonblock(void) {
-    nodelay(stdscr, true);
-    return getch();
-}
+void sleep_ms(int ms);
 
 int main(void) {
+    bool running = true;
+    WINDOW *board, *next, *hold, *score;
+
     srand(time(NULL));
 
     initscr();            // Initialize curses
@@ -23,9 +23,50 @@ int main(void) {
     keypad(stdscr, true); // Allow special keys like arrows
     start_color();        // Enable colors (create colors and color pairs)
     use_default_colors(); // Allow default terminal colors
-    refresh();            // Draw the screen
+    refresh();            // Draw the screen TODO: need this?
+
+    board = newwin(2, 2, 0, 5);
+    next = newwin(2, 2, 0, 8);
+    hold = newwin(2, 2, 0, 0);
+    score = newwin(2, 2, 4, 0);
+
+    while (running) {
+        display_board(board);
+        display_next(next);
+        display_hold(hold);
+        display_score(score);
+        doupdate();
+        sleep_ms(10);
+    }
 
     endwin();
 
+    printf("Game Over!\n");
+
     return 0;
+}
+
+void display_board(WINDOW *w) {
+    wnoutrefresh(w);
+}
+
+void display_next(WINDOW *w) {
+    wnoutrefresh(w);
+}
+
+void display_hold(WINDOW *w) {
+    wnoutrefresh(w);
+}
+
+void display_score(WINDOW *w) {
+    wnoutrefresh(w);
+}
+
+void sleep_ms(int ms) {
+    struct timespec ts;
+
+    ts.tv_sec = 0;
+    ts.tv_nsec = ms * 1000 * 1000;
+
+    nanosleep(&ts, NULL);
 }
