faa9cd6700a5d81df64932d32746fa389e6ee8fb
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-03-02 22:32:52 +1100

    feat(tetris): set up layout

--- tetris_ncurses/main.c
+++ tetris_ncurses/main.c
@@ -13,6 +13,7 @@ void sleep_ms(int ms);
 int main(void) {
     bool running = true;
     WINDOW *board, *next, *hold, *score;
+    int rows = 22, cols = 10;
 
     srand(time(NULL));
 
@@ -25,10 +26,10 @@ int main(void) {
     use_default_colors(); // Allow default terminal colors
     refresh();            // Draw the screen TODO: need this?
 
-    board = newwin(2, 2, 0, 5);
-    next = newwin(2, 2, 0, 8);
-    hold = newwin(2, 2, 0, 0);
-    score = newwin(2, 2, 4, 0);
+    board = newwin(rows + 2, cols * 2 + 2, 0, 0);
+    next = newwin(6, 10, 0, cols * 2 + 2 + 1);
+    hold = newwin(6, 10, 6, cols * 2 + 2 + 1);
+    score = newwin(6, 10, 12, cols * 2 + 2 + 1);
 
     while (running) {
         display_board(board);
@@ -37,28 +38,58 @@ int main(void) {
         display_score(score);
         doupdate();
         sleep_ms(10);
+
+        switch (getch()) {
+            case KEY_LEFT: {
+                break;
+            }
+            case KEY_RIGHT: {
+                break;
+            }
+            case KEY_UP: {
+                break;
+            }
+            case KEY_DOWN: {
+                break;
+            }
+            case 'q': {
+                running = false;
+                break;
+            }
+            case 'p': {
+                break;
+            }
+            case 'h': {
+                break;
+            }
+            default: {
+            }
+        }
     }
 
     endwin();
-
     printf("Game Over!\n");
 
     return 0;
 }
 
 void display_board(WINDOW *w) {
+    box(w, 0, 0);
     wnoutrefresh(w);
 }
 
 void display_next(WINDOW *w) {
+    box(w, 0, 0);
     wnoutrefresh(w);
 }
 
 void display_hold(WINDOW *w) {
+    box(w, 0, 0);
     wnoutrefresh(w);
 }
 
 void display_score(WINDOW *w) {
+    box(w, 0, 0);
     wnoutrefresh(w);
 }
 
