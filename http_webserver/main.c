329ad458b52880e1f12c3d95d5015797792b05f1
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-08-15 14:43:23 +1000

    feat(http_webserver): implement read and write

--- http_webserver/main.c
+++ http_webserver/main.c
@@ -1,11 +1,14 @@
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 2048
 
 //  CLIENT                             SERVER
-// 
+//
 // socket()                           socket()
 //    |                                  |
 //    | active                           | active
@@ -18,6 +21,14 @@
 //    +----------- connection -----------+
 
 int main(void) {
+    char buffer[BUFFER_SIZE];
+    char resp[] =
+        "HTTP/1.0 200 OK\r\n"
+        "Server: http-webserver-c\r\n"
+        "Content-Type: text/html\r\n\r\n"
+        "<html><head><title>HTTP Webserver in C</title></head><body>Hello, "
+        "World!</body></html>\r\n";
+
     // Create a socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
@@ -51,11 +62,41 @@ int main(void) {
     for (;;) {
         // Accept incoming connections
         int client_sockfd = accept(sockfd, (struct sockaddr *)&host_addr,
-                               (socklen_t *)&host_addrlen);
+                                   (socklen_t *)&host_addrlen);
         if (client_sockfd < 0) {
             perror("HTTP webserver (accept)");
         }
         printf("connection accepted\n");
+
+        // Get client address
+        struct sockaddr_in client_addr;
+        int client_addrlen = sizeof(client_addr);
+        int sockn = getsockname(client_sockfd, (struct sockaddr *)&client_addr,
+                                (socklen_t *)&client_addrlen);
+        if (sockn < 0) {
+            perror("HTTP webserver (getsockname)");
+            continue;
+        }
+
+        // Read from the socket
+        int valread = read(client_sockfd, buffer, BUFFER_SIZE);
+        if (valread < 0) {
+            perror("HTTP webserver (read)");
+            continue;
+        }
+
+        // Read teh request
+        char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
+        sscanf(buffer, "%s %s %s", method, uri, version);
+        printf("[%s:%u] %s %s %s\n", inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port), method, uri, version);
+
+        // Write to the socket
+        int valwrite = write(client_sockfd, resp, strlen(resp));
+        if (valwrite < 0) {
+            perror("HTTP webserver (write)");
+            continue;
+        }
 
         close(client_sockfd);
     }
